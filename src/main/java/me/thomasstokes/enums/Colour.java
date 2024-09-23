4ffb851e755ef43f0f2d6958337f8f00a3e15553
Thomas Stokes <thomas.stokes@sap.com>
2024-09-24 00:51:17 +0100

    Fix getrandom of colour enum to exclude colour none.

--- src/main/java/me/thomasstokes/enums/Colour.java
+++ src/main/java/me/thomasstokes/enums/Colour.java
@@ -11,12 +11,30 @@ public enum Colour {
     PURPLE,
     BLACK,
     NONE;
+
     public static Colour []VALUES = values();
+    public static Colour []VALUES_WITHOUT_NONE = setupValuesWithoutNone();
     private static Random RANDOM = new Random();
 
+    public static Colour[] setupValuesWithoutNone() {
+        Colour []allValues = values();
+        Colour []valuesExcludingNone = new Colour[allValues.length - 1];
+        int position = 0;
+        for (Colour colour : allValues) {
+            if (colour != Colour.NONE) {
+                valuesExcludingNone[position++] = colour;
+            }
+        }
+        return valuesExcludingNone;
+    }
+    /**
+     * 
+     * @return Returns random colour excluding None.
+     */
     public static Colour randomColour() {
-        int length = VALUES.length;
-        return VALUES[RANDOM.nextInt(length)];
+        int length = VALUES_WITHOUT_NONE.length;
+        return VALUES_WITHOUT_NONE[RANDOM.nextInt(length)];
+    
     }
 
 }
