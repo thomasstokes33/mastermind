89e49dd255df4493ce1fcfa3de5eab99344d8d71
Thomas Stokes <thomas.stokes@sap.com>
2024-09-24 01:01:43 +0100

    Setup colour picker to notify upon colour selection.

--- src/main/java/me/thomasstokes/component/ColourPicker.java
+++ src/main/java/me/thomasstokes/component/ColourPicker.java
@@ -1,11 +1,18 @@
 package me.thomasstokes.component;
+import java.util.ArrayList;
+import java.util.List;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import javafx.scene.control.Button;
-
 import javafx.scene.layout.VBox;
 
 import me.thomasstokes.enums.Colour;
+import me.thomasstokes.listeners.ColourPickedListener;
 public class ColourPicker extends VBox {
+    private final List<ColourPickedListener> colourChangedListeners;
+    private static final Logger logger = LogManager.getLogger(ColourPicker.class);
     public ColourPicker() {
+        colourChangedListeners = new ArrayList<>();
         getStyleClass().add("picker");
         build();
     }
@@ -18,8 +25,24 @@ public class ColourPicker extends VBox {
             var button = new Button();
             getChildren().add(button);
             // button.setStyle("-fx-background-color: " + Colour.VALUES[i].toString().toLowerCase());
-            
-            button.getStyleClass().add(Colour.VALUES[i].toString().toLowerCase());
+            Colour colour = Colour.VALUES[i];
+            button.getStyleClass().add(colour.toString().toLowerCase());
+            button.setOnMouseReleased(e -> colourChosen(colour));
         }
+    }
+    public void colourChosen(Colour colourPicked) {
+        logger.info("Color picked:" + colourPicked);
+        notifyColourPickedListeners(colourPicked);
+    }
+
+    public void notifyColourPickedListeners(Colour colourPicked) {
+        for (ColourPickedListener colourPickedListener : colourChangedListeners) {
+            colourPickedListener.colourPicked(colourPicked);
+        }
+    }
+
+    public ColourPickedListener addColourPickedListener(ColourPickedListener colourPickedListener) {
+        colourChangedListeners.add(colourPickedListener);
+        return colourPickedListener;
     }
 }
