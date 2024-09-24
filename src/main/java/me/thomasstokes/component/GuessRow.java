6e7180243d0e7371f37b12fef3530a30c7bfa0e1
Thomas Stokes <thomas.stokes@sap.com>
2024-09-24 01:16:21 +0100

    Finish constructor of guess row.

--- src/main/java/me/thomasstokes/component/GuessRow.java
+++ src/main/java/me/thomasstokes/component/GuessRow.java
@@ -1,6 +1,7 @@
 package me.thomasstokes.component;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -8,28 +9,41 @@ import org.apache.logging.log4j.Logger;
 import javafx.scene.control.Button;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.HBox;
-import javafx.scene.layout.Priority;
 import me.thomasstokes.App;
 import me.thomasstokes.enums.Colour;
+import me.thomasstokes.listeners.GuessConfirmedListener;
 
-
-public class GuessRow extends HBox{
+public class GuessRow extends HBox {
 
     private static final Logger logger = LogManager.getLogger(GuessRow.class);
-
+    private List<GuessConfirmedListener> guessConfirmedListeners = new ArrayList<>();
     private HBox guessArea;
-    private final ArrayList<Colour> guess; 
+    /**
+     * These are the buttons that act as the "pin holes", which change colour corresponding 
+     * to a user's guesses. 
+     */
+    private final List<Button> colourChangeButtons;
+    /**
+     * Array storing the current guess.
+     */
+    private final List<Colour> guess;
     private boolean locked = false;
     private boolean enabled = false;
-    /** 
+    /**
      * The selected position. Starts at 0.
      */
     private int selectedPosition = 0;
     public GuessRow() {
         guess = new ArrayList<>(4);
+        for (int i = 0; i < 4; i++) {
+            guess.add(Colour.NONE);
+        }
+        colourChangeButtons = new ArrayList<>();
         getStyleClass().add("guess-row");
         build();
+        setSelectedPosition(0);
     }
+
     public void build() {
         getChildren().clear();
 
