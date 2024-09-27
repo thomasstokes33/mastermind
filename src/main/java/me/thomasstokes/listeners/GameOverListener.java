f136d9fe49800dd1534fa0d940aac1e39441386b
Thomas Stokes <thomas.stokes@sap.com>
2024-09-27 13:29:49 +0100

    Add comments and javadoc for listeners

--- src/main/java/me/thomasstokes/listeners/GameOverListener.java
+++ src/main/java/me/thomasstokes/listeners/GameOverListener.java
@@ -4,6 +4,9 @@ import java.util.List;
 
 import me.thomasstokes.enums.Colour;
 
+/**
+ * Used by the Game to notify listeners that the game is over.
+ */
 public interface GameOverListener {
     public void gameOver(boolean isVictory, List<Colour> theHiddenAnswer);
 }
--- src/main/java/me/thomasstokes/listeners/GuessConfirmedListener.java
+++ src/main/java/me/thomasstokes/listeners/GuessConfirmedListener.java
@@ -5,7 +5,7 @@ import java.util.List;
 import me.thomasstokes.enums.Colour;
 
 /**
- * Used to indicate to a listener that a guess has been locked in.
+ * Used to notify a listener that a guess has been confirmed in a GuessRow.
  */
 public interface GuessConfirmedListener {
     void guessConfirmed(List<Colour> theConfirmedGuess);
